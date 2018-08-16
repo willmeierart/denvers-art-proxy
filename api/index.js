ead0219e32166ccf5dc3175e838bd03274c31745
WILL MEIER <willmeierart@gmail.com>
2018-08-15 21:56:41 -0600

    update

--- api/index.js
+++ api/index.js
@@ -69,11 +69,11 @@ router.get('/test', (req, res) => {
     const URL = `${baseURL}${fakeID}/${fakeFields}${fakeURLsuffix}`
     return fetch(URL)
       .then(res => res.json())
-      // .then(json => res.json(json))
-      // .catch(err => {
-      // console.log(err)
-      // return err
-    // })
+      .then(json => res.json(json))
+      .catch(err => {
+      console.log(err)
+      return err
+    })
   })()
 
   
