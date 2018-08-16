aeeb57253f1bd3b0137a5225116b98d7d2ccc9c9
WILL MEIER <willmeierart@gmail.com>
2018-08-15 20:44:09 -0600

    update server

--- api/index.js
+++ api/index.js
@@ -68,11 +68,11 @@ router.get('/test', (req, res) => {
   const getGalleryInfo = (() => {
     const URL = `${baseURL}${fakeID}/${fakeFields}${fakeURLsuffix}`
     return fetch(URL)
-      .then(res => res.json())
+      // .then(res => res.json())
       // .then(json => res.json(json))
-      .catch(err => {
-      console.log(err)
-      return
+      // .catch(err => {
+      // console.log(err)
+      // return err
     })
   })()
 
